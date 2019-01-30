e1f18375bca7d9121810854dd16636a7b140b029
lzfengluo <zhichao.zhang@speedatagroup>
2019-01-30 17:13:32 +0800

    UI控件练习——自动完成文本

---
+++ app/src/main/java/com/example/my/myuipractice/AutoTextActivity.java
@@ -0,0 +1,51 @@
+package com.example.my.myuipractice;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class AutoTextActivity extends Activity {
+
+    private AutoCompleteTextView autoText;
+    private Button searchBtn;
+
+    private static final String[] COUNTRIES = new String[]{
+            "明日科技",
+            "明日之后",
+            "明日之子",
+            "明日科技有限公司",
+            "河北省明日出版社",
+            "明日",
+            "ming ri",
+            "ming ri zhi hou",
+            "ming ri zhi zi",
+            "he bei sheng ming ri chu ban she"
+    };
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auto_text);
+        init();
+    }
+
+    private void init(){
+        autoText = (AutoCompleteTextView)findViewById(R.id.auto_atv);
+        searchBtn = (Button) findViewById(R.id.auto_search_btn);
+        // 为自动完成文本框设置适配器
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,COUNTRIES);
+        autoText.setAdapter(adapter);
+        // 为“搜索”按钮添加单击监听事件
+        searchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(AutoTextActivity.this,autoText.getText().toString(),Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/my/myuipractice/MainActivity.java
+++ app/src/main/java/com/example/my/myuipractice/MainActivity.java
@@ -1,13 +1,51 @@
 package com.example.my.myuipractice;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnAutoText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+
+    /**
+     * 初始化函数，获取控件id,并给按钮添加监听
+     */
+    private void init(){
+        btnAutoText = (Button) findViewById(R.id.btn_auto_text);
+        btnAutoText.setOnClickListener(new onClickBtn());
+    }
+
+    /**
+     * 处理监听事件
+     */
+    class onClickBtn implements View.OnClickListener{
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.btn_auto_text:
+                    openAct(AutoTextActivity.class);
+                    break;
+
+            }
+        }
+    }
+
+    /**打开一个activity页面，不传递数据
+     *
+     * @param actClass 传入要跳转的activity
+     */
+    public void openAct(Class<?> actClass){
+        Intent intent = new Intent(this,actClass);
+        startActivity(intent);
     }
 }
